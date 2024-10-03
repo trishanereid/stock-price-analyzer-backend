c409d28417ba252890cbdefd0bb5cafcab73409d
trishane <trishanereid@gmail.com>
2024-10-03 11:45:51 +0530

    Initial commit

---
+++ src/test/java/com/example/stock_price_analyzer/StockPriceAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_price_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPriceAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
